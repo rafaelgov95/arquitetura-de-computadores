c76d194c849a421923ebef4ef1c07ebfae3470fb
Mr.Viana <rafaelgov95@gmail.com>
2016-07-10 10:33:32 -0400

    up

--- Assembly/Mars/Codes/Trabalho I Kleber /Exercicio_3_d.asm
+++ Assembly/Mars/Codes/Trabalho I Kleber /Exercicio_3_d.asm
@@ -1,29 +1,32 @@
 .data
 
 .text
- add $v0,$zero,31123123132132132132
- move $t0,$v0
+li $v0,5
+syscall 
+
+move $t0,$v0
  
- beq $t0, 0xFFFFFFF,infinito
- jal finito
- j encerrar
+beq $t0, 0xFFFFFFF,infinito
+jal finito
+jal imp
+j encerrar
 
 
 infinito:
 add $t0,$zero,1
 move $s2,$t0
-jal imp
+jr $ra
 
 finito:
 add $t0,$zero,$zero 	
 move $s2,$t0
-jal imp
+jr $ra
 
 imp:
 li $v0,1
 la $a0, ($s2)
 syscall 
-	
+jr $ra	
 encerrar:
 li $v0,10
  
